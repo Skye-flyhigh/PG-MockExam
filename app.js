e33ce796a98b276ab10584666598b0de87cbfbdb
Skye <58520630+Skye-flyhigh@users.noreply.github.com>
2024-05-30 23:02:52 +0100

    Content update
    Exam content has been separated to another file.

--- app.js
+++ app.js
@@ -1,64 +1,3 @@
-const examContent = [{
-id: "P1",
-level: "P", 
-category: "POF",
-question: "If your paraglider suffers from a large asymmetric collapse at low level (300ft) and immediately starts to dive and rotate, the optimum pilot action is to:",
-solution: "a3",
-a1: "Apply opposite weight shift and strong opposite brake to try and stop the turn.",
-a2: "Deploy your emergency parachute.",
-a3: "Apply a small amount of opposite brake, and weight shift to try and slow or stop the turn, use deep pumps on the collapsed side to help the glider re-inflate.",
-a4: "Treat as a symmetrical collapse and pump both brakes deeply to counter the dive. Then steer out of the turn."
-}, {
-    id: "P2",
-    level: "P",
-    category: "POF",
-    question: "If a glider is flown at the top of its weight range its performance may be different to when it is flown at the bottom of its weight range. Some of the differences in performance caused by increasing the wing loading (but remaining within the recommended ranger) are likely to be:",
-    solution: "a2",
-    a1: "A reduction in sink rate, improvement in glide angle, an increased best glide airspeed.",
-    a2: "A slight increase in sink rate, no change to the glide angle, an increased best glide airspeed.",
-    a3: "No change to the sink rate, reduction in the glide angle, increased top speed.",
-    a4: "A reduction in sink rate, better glide angle, no change in top speed."
-}, {
-    id: "CP1",
-    level: "CP",
-    category: "Airlaw",
-    question: "You are ridge soaring and have the ridge on your left. You are approaching a hang glider head on. What should you do?",
-    solution: "a1",
-    a1: "Turn right",
-    a2: "Turn left",
-    a3: "Do nothing"
-}, {
-    id: "CP2",
-    level: "CP",
-    category: "Airlaw",
-    question: "You are on a converging course with another paraglider on your right hand side. You should:",
-    solution: "a1",
-    a1: "Move out of their way",
-    a2: "Hold your course",
-    a3: "Use big ears",
-    a3: "Stall the glider"
-} , {
-    id: "CP3",
-    level: "CP",
-    category: "Airlaw",
-    question: "You are coming in to land in a designated landing. It is a very busy with pilots above and below you. Which statement is correct?",
-    solution: "a2",
-    a1: "The higher pilot has right of way",
-    a2: "The lowest pilot on their final glide into land has right of way",
-    a3: "The fastest pilot has right of way",
-    a4: "The slowest pilot has right of way",
-    a5: "The best pilot has right of way"
-} , {
-    id: "CP4",
-    level: "CP",
-    category: "Airlaw",
-    question: "You are ridge soaring and wish to overtake another pilot ahead of you. Should you...",
-    solution: "a3",
-a1: "Overtake in between the ridge and the other glider",
-a2: "Overtake between the valley and the other glider",
-a3: "Overtake either side, being cautious of oncoming traffic"
-}];
-
 const questionContainer = document.getElementById("question-container");
 const submitBtn = document.querySelector(".submit-btn");
 const navbar = document.getElementById("nav");
@@ -135,48 +74,50 @@ function displayQuestion(num, level) {
          return arr;
       }
 
-    const arr = getRanArr(4); // this number will depend on the number of questions in a given level
+    const arr = getRanArr(5); // this number will depend on the number of questions in a given level
 
-    //Filter the questions to get the targeted level
-    const filteredQuestion = examContent.filter((question) => {return question.level === level})
+    //fetch the content from external JSON file
+    fetch('content.json')
+      .then(response => response.json())
+      .then(data => {
+            //Filter the questions to get the targeted level
+        const filteredQuestion = data.filter((question) => {return question.level === level})
 
-    // Create a question container for each of the selected question in a random order
-    for (let i = 0; i < numOfQuestions; i++) {
-        let item = filteredQuestion[arr[i]];
-        const element = document.createElement("article");
-        let attr = document.createAttribute("data-id");
-        attr.value = item.id;
-        element.setAttributeNode(attr);
-        element.classList.add("bg-slate-300", "rounded-md", "border", "px-5", "py-5", "flex", "flex-col", "gap-4", "justify-center", "max-w-2xl", "m-2", "dark:bg-slate-800", "dark:text-white", "dark:border-gray-600");
-        
-        element.innerHTML = `<!-- question start -->
-        <h3 id="question-${i + 1}" class="font-semibold sm:text-lg text-base">${i + 1}. ${item.question}</h3>
-        <div id="answer-container-${i + 1}" class="bg-white rounded-md p-4 gap-3 flex flex-col sm:text-base text-sm dark:bg-slate-900 dark:text-white">
-        </div>
-        <!-- question end -->
-        `;
-        questionContainer.appendChild(element);
-
-        // display as many answers as there are in the database, assuming the database has always the same structure.
-        let answersKeys = Object.keys(item).slice(5);
-        let answers = Object.values(item).slice(5);
-
-        let answerContainer = document.getElementById(`answer-container-${i + 1}`);
-
-        answersKeys.forEach((answerKey) => {
-            const answer = document.createElement("div");
-            const index = answersKeys.indexOf(answerKey);
-            answer.setAttribute("id", answerKey )
-            answer.innerHTML = `
-            <input type="radio" id="${answerKey}-${i + 1}" name="${item.id}" value="${answerKey}" class="radio-answer mr-1">
-            <label for="${answerKey}-${i + 1}" class="${answerKey}">${answers[index]}</label>
+        // Create a question container for each of the selected question in a random order
+        for (let i = 0; i < numOfQuestions; i++) {
+            let item = filteredQuestion[arr[i]];
+            const element = document.createElement("article");
+            let attr = document.createAttribute("data-id");
+            attr.value = item.id;
+            element.setAttributeNode(attr);
+            element.classList.add("bg-slate-300", "rounded-md", "border", "px-5", "py-5", "flex", "flex-col", "gap-4", "justify-center", "m-2", "dark:bg-slate-800", "dark:text-white", "dark:border-gray-600", "w-11/12", "max-w-2xl");
+            
+            element.innerHTML = `<!-- question start -->
+            <h3 id="question-${i + 1}" class="font-semibold sm:text-lg text-base">${i + 1}. ${item.question}</h3>
+            <div id="answer-container-${i + 1}" class="bg-white rounded-md p-4 gap-3 flex flex-col sm:text-base text-sm dark:bg-slate-900 dark:text-white">
+            </div>
+            <!-- question end -->
             `;
-            answerContainer.appendChild(answer)
-        })
-    }
-};
+            questionContainer.appendChild(element);
 
-// ******************** Submit *****************************
+            // display as many answers as there are in the database, assuming the database has always the same structure.
+            let answersKeys = Object.keys(item).slice(5);
+            let answers = Object.values(item).slice(5);
+
+            let answerContainer = document.getElementById(`answer-container-${i + 1}`);
+
+            answersKeys.forEach((answerKey) => {
+                const answer = document.createElement("div");
+                const index = answersKeys.indexOf(answerKey);
+                answer.setAttribute("id", answerKey )
+                answer.innerHTML = `
+                <input type="radio" id="${answerKey}-${i + 1}" name="${item.id}" value="${answerKey}" class="radio-answer mr-1">
+                <label for="${answerKey}-${i + 1}" class="${answerKey}">${answers[index]}</label>
+                `;
+                answerContainer.appendChild(answer)
+            })
+        }
+        // ******************** Submit *****************************
 //Submit to validate the answer and check the write from wrong
 submitBtn.addEventListener("click", () => {
     let amountCorrect = 0;
@@ -194,7 +135,7 @@ submitBtn.addEventListener("click", () => {
         let myRadios = document.getElementsByName(selectedQuestions[i]);
 
         // find the right answer
-        let solutionValue = examContent.find(x => x.id === selectedQuestions[i]).solution;
+        let solutionValue = data.find(x => x.id === selectedQuestions[i]).solution;
 
         //loop to check & mark answers for selected radios
         for (let j = 0; j < myRadios.length; j++) {
@@ -218,6 +159,10 @@ submitBtn.addEventListener("click", () => {
     score.classList.add("bg-green-200", "dark:bg-green-600") : 
     score.classList.add("bg-red-200", "dark:bg-red-600");
 })
+    })
+};
+
+
 
 //Function to clear the previous questions and score
 function reset() {
